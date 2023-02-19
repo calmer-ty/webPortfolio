37a76b1bc6237829a0e8531a008fefa76beccb78
calm <ty.calmer96@gmail.com>
2023-02-19 13:37:14 +0900

    스마트폰 레이아웃 / cooper 섹션 효과 추가

--- js/index.js
+++ js/index.js
@@ -1,9 +1,54 @@
 const contPortfolio = document.querySelectorAll(".cont.portfolio");
 const contType1 = document.querySelector(".cont.type1");
 const contType2 = document.querySelector(".cont.type2");
+
 const listItem = contPortfolio[0].querySelector(".list-item ul");
+const listItem1 = contPortfolio[1].querySelector(".list-item ul");
 
 let itemCount = 0;
+
+// 포트폴리오 목록 슬라이드 0
+function itemSlide() {
+  const item = listItem.querySelectorAll("li");
+  const itemWidth = item[0].offsetWidth;
+  const btnMove = contPortfolio[0].querySelectorAll(".btn-move button");
+
+  btnMove[0].addEventListener("click", function () {
+    itemCount--;
+    if (itemCount < 0) {
+      itemCount = 0;
+    }
+    listItem.style.left = "-" + itemCount * itemWidth + "px";
+  });
+  btnMove[1].addEventListener("click", function () {
+    itemCount++;
+    if (itemCount > item.length - 1) {
+      itemCount = item.length - 1;
+    }
+    listItem.style.left = "-" + itemCount * itemWidth + "px";
+  });
+}
+// 포트폴리오 목록 슬라이드 1
+function itemSlide1() {
+  const item = listItem1.querySelectorAll("li");
+  const itemWidth = item[0].offsetWidth;
+  const btnMove = contPortfolio[1].querySelectorAll(".btn-move button");
+
+  btnMove[0].addEventListener("click", function () {
+    itemCount--;
+    if (itemCount < 0) {
+      itemCount = 0;
+    }
+    listItem1.style.left = "-" + itemCount * itemWidth + "px";
+  });
+  btnMove[1].addEventListener("click", function () {
+    itemCount++;
+    if (itemCount > item.length - 1) {
+      itemCount = item.length - 1;
+    }
+    listItem1.style.left = "-" + itemCount * itemWidth + "px";
+  });
+}
 
 function infoSlide() {
   const contInfo = document.querySelectorAll(".cont");
@@ -19,6 +64,7 @@ function infoSlide() {
     }
     switchPage();
     listItem.style.left = "0px";
+    listItem1.style.left = "0px";
     itemCount = 0;
   }
   // 스크롤 내릴 때
@@ -29,6 +75,7 @@ function infoSlide() {
     }
     switchPage();
     listItem.style.left = "0px";
+    listItem1.style.left = "0px";
     itemCount = 0;
   }
   // 스크롤마다 클래스 추가/제거
@@ -41,7 +88,6 @@ function infoSlide() {
       }
     }
   }
-
   // 키로 슬라이드 작동
   document.addEventListener("keydown", function (event) {
     if (event.key === "ArrowUp") {
@@ -76,36 +122,9 @@ function infoSlide() {
   }
 }
 
-function itemSlide() {
-  const item = listItem.querySelectorAll("li");
-  const itemWidth = item[0].offsetWidth;
-  const btnMove = contPortfolio[0].querySelectorAll(".btn-move button");
-
-  function calculationSlide() {
-    listItem.style.left = "-" + itemCount * itemWidth + "px";
-  }
-
-  btnMove[0].addEventListener("click", function () {
-    itemCount--;
-    if (itemCount < 0) {
-      itemCount = 0;
-    }
-    calculationSlide();
-  });
-  btnMove[1].addEventListener("click", function () {
-    itemCount++;
-    if (itemCount > item.length - 1) {
-      itemCount = item.length - 1;
-    }
-    calculationSlide();
-  });
-}
-
-// window.addEventListener("resize", function () {
-//   itemSlide();
-// });
 function init() {
   infoSlide();
   itemSlide();
+  itemSlide1();
 }
 init();
